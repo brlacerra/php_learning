fe0fba50c949673f78c79ea76c786e26fd25c645
futoibrunao <brufecerra@gmail.com>
2023-02-21 11:16:19 -0300

    json e constantes

--- 04-Arrays/02-json.php
+++ 04-Arrays/02-json.php
@@ -7,6 +7,13 @@ array_push($pessoas, array('nome' => 'Glaucio', 'idade' => 35));
 
 // print_r($pessoas);
 
+// Tranforma Array em json
 echo json_encode($pessoas);
+
+//Transforma json em Array
+$json = '[{"nome":"Jo\u00e3o","idade":25},{"nome":"Glaucio","idade":35}]';
+$data = json_decode($json, true);
+
+var_dump($data);
 
 ?>
---
+++ 04-Arrays/03-constante.php
@@ -0,0 +1,19 @@
+<?php
+
+define("SERVIDOR", "127.0.0.1");
+echo SERVIDOR;
+
+echo "<br>";
+
+define("BANCO_DE_DADOS", ['127.0.0.1', 'root', 'password', 'test']);
+print_r (BANCO_DE_DADOS);
+
+echo "<br>";
+
+echo PHP_VERSION;
+
+echo "<br>";
+
+ECHO DIRECTORY_SEPARATOR;
+
+?>
